082892dc768864a7441987f757963e04d2fb3600
BryanSemeniuk <bryansemeniuk@gmail.com>
2014-03-07 18:09:01 -0800

    Create distance.java

---
+++ distance.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package p1;
+import java.util.Scanner;
+
+/**
+ * @author Bryan Semeniuk
+ *
+ */
+public class distance {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        double x1, y1, x2, y2;  //coordinates of two points
+        double distance;        //  distance between the points
+        
+        Scanner scan = new Scanner(System.in);
+        
+        //Read in the two points
+        System.out.print ("Enter the coordinates of the first point " + 
+        "(put a space between them): ");
+        
+        x1 = scan.nextDouble();
+        y1 = scan.nextDouble();
+        
+        System.out.print ("Enter the coordinates of the second point: ");
+        x2 = scan.nextDouble();
+        y2 = scan.nextDouble();
+        
+        //Compute the Distance
+        
+        distance = Math.sqrt((Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)));
+        
+        //Print out the answer
+        
+        System.out.println("The distance between your two points is " + distance);
+    }
+
+}
